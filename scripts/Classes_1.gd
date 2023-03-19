c8fb2e105c6cfbe47c4ba36296faf5c938dd5e03
Pe3Hu <34793005+Pe3Hu@users.noreply.github.com>
2023-03-19 15:07:28 +0300

    version 0.000
    1. empty project

---
+++ scripts/Classes_1.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+class Orbit:
+	var obj = {}
+
+
+	func _init(input_) -> void:
+		pass
+
+
+class Anker:
+	var obj = {}
+
+
+	func _init(input_) -> void:
+		pass
+
+
+class Stadion:
+	var word = {}
+	var vec = {}
+	var dict = {}
+	var arr = {}
+
+
+	func _init() -> void:
+		dict.verein = {}
